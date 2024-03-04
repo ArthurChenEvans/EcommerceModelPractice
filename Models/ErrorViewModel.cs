a05e278e6c94ecf34606a4c8c917f6429a3c3d4b
ArthurChenEvans <arthurchenevans@protonmail.com>
2024-03-04 17:14:03 +0000

    Added local repo to remote repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace E_commerse_Model_Practice.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
