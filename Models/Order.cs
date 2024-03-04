298df37ce957cc1c227ad4f4b1973d29cb5ed11a
ArthurChenEvans <arthurchenevans@protonmail.com>
2024-03-04 17:55:17 +0000

    Added required error messages

--- Models/Order.cs
+++ Models/Order.cs
@@ -6,11 +6,11 @@ public class Order : IValidatableObject
 {
     [Range(minimum:1, maximum:99999, ErrorMessage = "Order Number Out Of Range (1-99999)")]
     public int? OrderNumber { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Order date can't be blank.")]
     public DateTime OrderDate { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Must have at least one product in your list of products.")]
     public List<Product> Products { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Invoice price cannot be blank.")]
     public double InvoicePrice { get; set; }
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
